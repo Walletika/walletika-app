aa1fa1d307150766fd3cb2fa327f8c110d036c6a
MahmoudKhalid <darkness20016@gmail.com>
2022-10-21 16:52:28 +0200

    Edit logo size and capitalize app name

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.CreateAndShow(L"walletika", origin, size)) {
+  if (!window.CreateAndShow(L"Walletika", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
