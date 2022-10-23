fb7e6a0ee0f04f37a8e8d3b3ee84a14f0d86c2ee
MahmoudKhalid <darkness20016@gmail.com>
2022-10-23 14:42:18 +0200

    Move wallet repo files into folder

--- lib/controllers/wallet_manager.dart
+++ lib/controllers/wallet_manager.dart
@@ -2,8 +2,8 @@ import 'package:get/get.dart';
 import 'package:walletika_sdk/walletika_sdk.dart';
 
 import '../models/wallet.dart';
-import '../repositories/wallet_fake_repo.dart';
-import '../repositories/wallet_repo.dart';
+import '../repositories/wallet/wallet_fake_repo.dart';
+import '../repositories/wallet/wallet_repo.dart';
 
 class WalletManagerController extends GetxController {
   // Data repository
