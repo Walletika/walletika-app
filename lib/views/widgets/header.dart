b7e3fff4a68792ccca3c718b693cb3489b42bfbd
MahmoudKhalid <darkness20016@gmail.com>
2022-10-25 18:51:32 +0200

    MainHeader directionality always left to right

--- lib/views/widgets/header.dart
+++ lib/views/widgets/header.dart
@@ -22,62 +22,65 @@ class MainHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      toolbarHeight: height,
-      backgroundColor: backgroundTransparent ? Colors.transparent : null,
-      leadingWidth: backButtonVisible ? 105.0 : 60.0,
-      leading: Row(
-        children: [
-          horizontalSpace(15.0),
-          backButtonVisible
-              ? IconButton(
-                  onPressed: Get.back,
-                  tooltip: "Back".tr,
-                  icon: const Icon(
-                    Icons.arrow_back_ios_rounded,
-                    size: AppDecoration.iconSmallSize,
-                  ),
-                )
-              : zeroSpace(),
-          brandVisible
-              ? GestureDetector(
-                  onTap: () => Get.until((route) => AppPages.isHome),
-                  child: MouseRegion(
-                    cursor: SystemMouseCursors.click,
-                    child: Tooltip(
-                      message: "Home".tr,
-                      child: Image.asset(
-                        AppImages.logo,
-                        width: 40.0,
-                        filterQuality: FilterQuality.medium,
-                        isAntiAlias: true,
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: AppBar(
+        toolbarHeight: height,
+        backgroundColor: backgroundTransparent ? Colors.transparent : null,
+        leadingWidth: backButtonVisible ? 105.0 : 60.0,
+        leading: Row(
+          children: [
+            horizontalSpace(15.0),
+            backButtonVisible
+                ? IconButton(
+                    onPressed: Get.back,
+                    tooltip: "Back".tr,
+                    icon: const Icon(
+                      Icons.arrow_back_ios_rounded,
+                      size: AppDecoration.iconSmallSize,
+                    ),
+                  )
+                : zeroSpace(),
+            brandVisible
+                ? GestureDetector(
+                    onTap: () => Get.until((route) => AppPages.isHome),
+                    child: MouseRegion(
+                      cursor: SystemMouseCursors.click,
+                      child: Tooltip(
+                        message: "Home".tr,
+                        child: Image.asset(
+                          AppImages.logo,
+                          width: 40.0,
+                          filterQuality: FilterQuality.medium,
+                          isAntiAlias: true,
+                        ),
                       ),
                     ),
-                  ),
-                )
-              : zeroSpace(),
-        ],
+                  )
+                : zeroSpace(),
+          ],
+        ),
+        title: brandVisible
+            ? Text(
+                AppInfo.name,
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22.0,
+                    ),
+              )
+            : null,
+        actions: actionsVisible
+            ? [
+                IconButton(
+                  padding: EdgeInsets.zero,
+                  tooltip: "Settings".tr,
+                  onPressed: () => Get.toNamed(AppPages.settings),
+                  icon: const Icon(LineIcons.cog),
+                ),
+                horizontalSpace(),
+              ]
+            : null,
       ),
-      title: brandVisible
-          ? Text(
-              AppInfo.name,
-              style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 22.0,
-                  ),
-            )
-          : null,
-      actions: actionsVisible
-          ? [
-              IconButton(
-                padding: EdgeInsets.zero,
-                tooltip: "Settings".tr,
-                onPressed: () => Get.toNamed(AppPages.settings),
-                icon: const Icon(LineIcons.cog),
-              ),
-              horizontalSpace(),
-            ]
-          : null,
     );
   }
 }
