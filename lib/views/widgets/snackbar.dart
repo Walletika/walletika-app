2a82d5b5a4960e29d7410cf460c0ada1fddeaff7
Mahmoud Khalid <darkness20016@gmail.com>
2022-11-08 16:12:56 +0200

    Added modernSnackBar widget

---
+++ lib/views/widgets/snackbar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:line_icons/line_icons.dart';
+
+import '../../utils/constants.dart';
+
+SnackbarController modernSnackBar({
+  required BuildContext context,
+  required String title,
+  required String message,
+  Icon? icon,
+  bool? isSuccess,
+  double maxWidth = 500.0,
+  void Function(GetSnackBar)? onTap,
+}) {
+  if (icon == null && isSuccess is bool) {
+    icon = isSuccess
+        ? const Icon(LineIcons.check, color: AppColors.green, size: 30.0)
+        : const Icon(LineIcons.times, color: AppColors.red, size: 30.0);
+  }
+
+  return Get.snackbar(
+    '',
+    '',
+    backgroundColor: AppColors.background3.withOpacity(0.5),
+    leftBarIndicatorColor: icon?.color,
+    titleText: Text(title, style: Theme.of(context).textTheme.titleLarge),
+    messageText: Text(message, style: Theme.of(context).textTheme.bodyMedium),
+    icon: icon,
+    maxWidth: maxWidth,
+    padding: const EdgeInsets.all(AppDecoration.padding),
+    margin: const EdgeInsets.all(AppDecoration.margin),
+    borderRadius: AppDecoration.radius,
+    onTap: onTap,
+  );
+}
