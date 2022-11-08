a86887ef8401dafa5e7bdef99c3192bfb0c24e61
Mahmoud Khalid <darkness20016@gmail.com>
2022-11-08 16:11:17 +0200

    Added awesomeDialog widget

---
+++ lib/views/widgets/dialog.dart
@@ -0,0 +1,58 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
+
+import '../../utils/constants.dart';
+
+AwesomeDialog awesomeDialog({
+  required BuildContext context,
+  double? width = 500.0,
+  DialogType dialogType = DialogType.noHeader,
+  String? title,
+  String? desc,
+  Widget? body,
+  String? btnOkText,
+  void Function()? btnOkOnPress,
+  String? btnCancelText,
+  void Function()? btnCancelOnPress,
+  bool autoDismiss = true,
+}) {
+  return AwesomeDialog(
+    context: context,
+    width: width,
+    dialogBackgroundColor: Theme.of(context).backgroundColor,
+    padding: const EdgeInsets.all(AppDecoration.padding),
+    dialogType: dialogType,
+    animType: AnimType.rightSlide,
+    headerAnimationLoop: false,
+    showCloseIcon: true,
+    closeIcon: const Icon(LineIcons.times),
+    title: title,
+    desc: desc,
+    body: body,
+    btnOk: btnOkText is String
+        ? SizedBox(
+            height: 35.0,
+            child: ElevatedButton(
+              onPressed: () {
+                if (btnOkOnPress != null) btnOkOnPress();
+                if (autoDismiss) Navigator.pop(context);
+              },
+              child: Text(btnOkText),
+            ),
+          )
+        : null,
+    btnCancel: btnCancelText is String
+        ? SizedBox(
+            height: 35.0,
+            child: ElevatedButton(
+              onPressed: () {
+                if (btnCancelOnPress != null) btnCancelOnPress();
+                if (autoDismiss) Navigator.pop(context);
+              },
+              child: Text(btnCancelText),
+            ),
+          )
+        : null,
+  );
+}
