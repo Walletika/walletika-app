6a623ec28bd34ac77a61becbb712e94531675996
Mahmoud Khalid <darkness20016@gmail.com>
2022-11-09 12:08:17 +0200

    SettingsView improvement

--- lib/views/settings/body.dart
+++ lib/views/settings/body.dart
@@ -17,128 +17,97 @@ class SettingsBody extends StatelessWidget {
       fit: StackFit.expand,
       children: [
         // Wallpaper image
-        ClipRRect(
-          borderRadius: const BorderRadius.vertical(
-            top: Radius.circular(40.0),
-          ),
-          child: Obx(() {
-            return Image.asset(
-              _settingsController.isDarkMode
-                  ? AppImages.nightIllustrations
-                  : AppImages.dayIllustrations,
-              filterQuality: FilterQuality.medium,
-              isAntiAlias: true,
-              fit: BoxFit.cover,
-            );
-          }),
-        ),
+        Obx(() {
+          return Image.asset(
+            _settingsController.isDarkMode
+                ? AppImages.nightIllustrations
+                : AppImages.dayIllustrations,
+            filterQuality: FilterQuality.medium,
+            isAntiAlias: true,
+            fit: BoxFit.cover,
+          );
+        }),
         // Content
-        Column(
-          children: [
-            // Space to appear the wallpaper image
-            const Expanded(child: SizedBox()),
-            // Content body
-            Expanded(
-              flex: 2,
-              child: Container(
-                padding: const EdgeInsets.only(
-                  left: AppDecoration.padding,
-                  top: AppDecoration.padding,
-                  right: AppDecoration.padding,
+        Container(
+          padding: const EdgeInsets.all(AppDecoration.padding),
+          color: Theme.of(context).backgroundColor.withOpacity(0.7),
+          child: ListView(
+            children: [
+              ListTile(
+                leading: const Icon(LineIcons.cog),
+                horizontalTitleGap: 0,
+                title: Text(
+                  "1000@settings".tr,
+                  style: Theme.of(context).textTheme.titleLarge,
                 ),
-                constraints: const BoxConstraints.expand(),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).backgroundColor.withOpacity(0.9),
-                  borderRadius: const BorderRadius.vertical(
-                    top: Radius.circular(40.0),
-                  ),
+              ),
+              ListTile(
+                title: Text("1001@settings".tr),
+                trailing: Obx(() {
+                  return Switch.adaptive(
+                    value: _settingsController.isDarkMode,
+                    onChanged: _settingsController.darkModeUpdate,
+                  );
+                }),
+              ),
+              ListTile(
+                title: Text("1002@settings".tr),
+                trailing: Obx(() {
+                  return DropdownButton(
+                    icon: const Icon(Icons.arrow_drop_down_rounded),
+                    borderRadius: BorderRadius.circular(AppDecoration.radius),
+                    dropdownColor: Theme.of(context).backgroundColor,
+                    underline: zeroSpace(),
+                    value: _settingsController.currentLanguage,
+                    onChanged: _settingsController.languageUpdate,
+                    items: _settingsController.languages
+                        .map((key, value) {
+                          return MapEntry(
+                            key,
+                            DropdownMenuItem(value: key, child: Text(value)),
+                          );
+                        })
+                        .values
+                        .toList(),
+                  );
+                }),
+              ),
+              ListTile(
+                onTap: () => Get.toNamed(AppPages.network),
+                title: Text("1003@settings".tr),
+                subtitle: Obx(() => Text(_settingsController.currentNetwork)),
+                trailing: const Icon(
+                  Icons.arrow_forward_ios_rounded,
+                  size: AppDecoration.iconSmallSize,
                 ),
-                child: ListView(
+              ),
+              const Divider(
+                indent: AppDecoration.dividerPadding,
+                endIndent: AppDecoration.dividerPadding,
+              ),
+              ListTile(
+                title: Text("1004@settings".tr),
+                subtitle: ButtonBar(
                   children: [
-                    ListTile(
-                      leading: const Icon(LineIcons.cog),
-                      horizontalTitleGap: 0,
-                      title: Text(
-                        "1000@settings".tr,
-                        style: Theme.of(context).textTheme.titleLarge,
+                    SizedBox(
+                      height: 35.0,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text("1005@settings".tr),
                       ),
                     ),
-                    ListTile(
-                      title: Text("1001@settings".tr),
-                      trailing: Obx(() {
-                        return Switch.adaptive(
-                          value: _settingsController.isDarkMode,
-                          onChanged: _settingsController.darkModeUpdate,
-                        );
-                      }),
-                    ),
-                    ListTile(
-                      title: Text("1002@settings".tr),
-                      trailing: Obx(() {
-                        return DropdownButton(
-                          icon: const Icon(Icons.arrow_drop_down_rounded),
-                          borderRadius: BorderRadius.circular(
-                            AppDecoration.radius,
-                          ),
-                          dropdownColor: Theme.of(context).backgroundColor,
-                          underline: zeroSpace(),
-                          value: _settingsController.currentLanguage,
-                          onChanged: _settingsController.languageUpdate,
-                          items: _settingsController.languages
-                              .map((key, value) {
-                                return MapEntry(
-                                    key,
-                                    DropdownMenuItem(
-                                      value: key,
-                                      child: Text(value),
-                                    ));
-                              })
-                              .values
-                              .toList(),
-                        );
-                      }),
-                    ),
-                    ListTile(
-                      onTap: () => Get.toNamed(AppPages.network),
-                      title: Text("1003@settings".tr),
-                      subtitle: Obx(() => Text(
-                            _settingsController.currentNetwork,
-                          )),
-                      trailing: const Icon(
-                        Icons.arrow_forward_ios_rounded,
-                        size: AppDecoration.iconSmallSize,
-                      ),
-                    ),
-                    const Divider(
-                      indent: AppDecoration.dividerPadding,
-                      endIndent: AppDecoration.dividerPadding,
-                    ),
-                    ListTile(
-                      title: Text("1004@settings".tr),
-                      subtitle: ButtonBar(
-                        children: [
-                          SizedBox(
-                            height: 35.0,
-                            child: ElevatedButton(
-                              onPressed: () {},
-                              child: Text("1005@settings".tr),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 35.0,
-                            child: ElevatedButton(
-                              onPressed: () {},
-                              child: Text("1006@settings".tr),
-                            ),
-                          ),
-                        ],
+                    SizedBox(
+                      height: 35.0,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text("1006@settings".tr),
                       ),
                     ),
                   ],
                 ),
               ),
-            )
-          ],
+            ],
+          ),
         ),
       ],
     );
