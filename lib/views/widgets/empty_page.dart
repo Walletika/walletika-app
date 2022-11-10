11d4004e42239ad1a575f55c7f78e6a2b71e7de3
Mahmoud Khalid <darkness20016@gmail.com>
2022-11-10 06:01:23 +0200

    Added EmptyPage widget

---
+++ lib/views/widgets/empty_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../../utils/constants.dart';
+import 'spacer.dart';
+
+class EmptyPage extends StatelessWidget {
+  const EmptyPage({
+    required this.illustrationPath,
+    required this.title,
+    required this.desc,
+    super.key,
+  });
+
+  final String illustrationPath;
+  final String title;
+  final String desc;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(AppDecoration.padding),
+      child: Column(
+        children: [
+          Flexible(
+            flex: 8,
+            child: Image.asset(
+              illustrationPath,
+              filterQuality: FilterQuality.medium,
+              isAntiAlias: true,
+            ),
+          ),
+          Flexible(
+            child: Text(
+              title,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
+          verticalSpace(5.0),
+          Flexible(
+            child: Text(
+              desc,
+              style: Theme.of(context).textTheme.labelMedium,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
