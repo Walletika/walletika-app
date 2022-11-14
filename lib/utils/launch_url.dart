f244e283e048118b1d51c37fecf6a6020c52df31
Mahmoud Khalid <darkness20016@gmail.com>
2022-11-14 14:53:56 +0200

    Added urlCorrection method

--- lib/utils/launch_url.dart
+++ lib/utils/launch_url.dart
@@ -9,3 +9,20 @@ Future<void> launchURL(String url) async {
     throw Exception("Could not launch $url");
   }
 }
+
+String urlCorrection({required String url, String protocol = 'http'}) {
+  url = url.replaceAll(' ', '');
+
+  while (url.contains('//')) {
+    url = url.replaceAll('//', '/');
+  }
+
+  if (url.startsWith('/')) url = url.substring(1, url.length);
+
+  if (url.endsWith('/')) url = url.substring(0, url.length - 1);
+
+  final List<String> urlSplit = url.split(':/');
+  if (urlSplit.length == 1) urlSplit.insert(0, protocol);
+
+  return "${urlSplit[0]}://${urlSplit[1]}";
+}
