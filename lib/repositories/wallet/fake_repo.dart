5d12174db8c09b1f67b0effe5738e4535f0351e1
Mahmoud Khalid <darkness20016@gmail.com>
2022-11-23 17:07:00 +0200

    Edit fake wallets data

--- lib/repositories/wallet/fake_repo.dart
+++ lib/repositories/wallet/fake_repo.dart
@@ -8,16 +8,9 @@ import 'repo.dart';
 
 class WalletFakeRepository extends WalletRepository {
   final Map<String, String> _wallets = {
-    'Wallet 1': '0x6Aa0594dCDBb1567533Bb993EF3ef61291725141',
-    'Wallet 2': '0x80A12649298e565600cE4Dd2497cdD749Cd0720d',
-    'Wallet 3': '0xaD923b04b2B08D678668D09D9776968cE1811F66',
-    // 'Wallet 4': '0xFE96aB9a007cdc6283566803d29C73e0F4CdF0cB',
-    // 'Wallet 5': '0xeD3eD2E44CA62D0343B8780A2e13a7E707E1f422',
-    // 'Wallet 6': '0xC7574ECB3265B4AB79431107a3dfB560a4b344b4',
-    // 'Wallet 7': '0x39b960758c6473c5aA77D30FEBb7A29987bfB852',
-    // 'Wallet 8': '0x5D3b75341d991e82877289A286F5071691f50e56',
-    // 'Wallet 9': '0x80De2f41F2DBf2FA8e439F3f3C2dA9aA2759B5f6',
-    // 'Wallet 10': '0x4F2558eEC33c04d2D0dDF96ee2b9faeb13B19dde',
+    'Wallet 1': '0x1755cceE2BCb80fb7f453afa523F2C13f024E1B5',
+    'Wallet 2': '0xA9e3105c94F99b570e538F012B0E4944C0C7dD58',
+    'Wallet 3': '0x8357648712DE4DB19BF17E27b8f10b7d16A3a5Bb',
   };
 
   @override
@@ -34,7 +27,7 @@ class WalletFakeRepository extends WalletRepository {
               username: username,
               securityPassword: Uint8List(0),
               dateCreated: DateTime.now(),
-              isFavorite: i <= 2 ? true : false,
+              isFavorite: i < 2 ? true : false,
             ),
           );
 
@@ -70,7 +63,7 @@ class WalletFakeRepository extends WalletRepository {
     required String address,
     required String password,
   }) async {
-    return password == '123456789';
+    return password == '123';
   }
 
   @override
@@ -79,7 +72,7 @@ class WalletFakeRepository extends WalletRepository {
     required String password,
     required String otpCode,
   }) async {
-    return password == '123456789' && otpCode == '123456';
+    return password == '123';
   }
 
   @override
