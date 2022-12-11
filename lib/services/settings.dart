68a2132b75f798325f23633a259166152a0a51de
Mahmoud Khalid <darkness20016@gmail.com>
2022-12-11 21:37:11 +0200

    SettingsService upgrade

--- lib/services/settings.dart
+++ lib/services/settings.dart
@@ -1,51 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:path_provider/path_provider.dart';
 
 import '../locales/locales.dart';
 import '../utils/constants.dart';
 
 class SettingsService extends GetxService {
-  final GetStorage storage = GetStorage(AppInfo.name);
   static const String _darkModeKey = 'darkMode';
   static const String _lanKey = 'language';
   static const String _networkKey = 'network';
   static const String _testnetHiddenKey = 'testnetHidden';
 
-  late bool isDarkMode;
-  late String currentLanguage;
-  late String currentNetwork;
-  late bool isTestnetHidden;
+  late GetStorage _storage;
+  late bool _isDarkMode;
+  late String _currentLanguage;
+  late String _currentNetwork;
+  late bool _isTestnetHidden;
 
-  ThemeMode get theme => isDarkMode ? ThemeMode.dark : ThemeMode.light;
+  bool get isDarkMode => _isDarkMode;
 
-  Locale get locale => Locale(currentLanguage);
+  String get currentLanguage => _currentLanguage;
+
+  String get currentNetwork => _currentNetwork;
+
+  bool get isTestnetHidden => _isTestnetHidden;
+
+  ThemeMode get theme => _isDarkMode ? ThemeMode.dark : ThemeMode.light;
+
+  Locale get locale => Locale(_currentLanguage);
 
   Future<SettingsService> init() async {
-    isDarkMode = storage.read(_darkModeKey) ?? false;
-    currentLanguage = storage.read(_lanKey) ?? AppTranslator.locales.keys.first;
-    currentNetwork = storage.read(_networkKey) ?? AppInfo.network;
-    isTestnetHidden = storage.read(_testnetHiddenKey) ?? true;
+    _storage = GetStorage(
+      AppInfo.name,
+      await getApplicationSupportDirectory().then((dir) => dir.path),
+    );
+
+    _isDarkMode = _storage.read(_darkModeKey) ?? false;
+    _currentLanguage =
+        _storage.read(_lanKey) ?? AppTranslator.locales.keys.first;
+    _currentNetwork = _storage.read(_networkKey) ?? AppInfo.network;
+    _isTestnetHidden = _storage.read(_testnetHiddenKey) ?? true;
+
     return this;
   }
 
   Future<void> darkModeUpdate(bool enabled) async {
-    await storage.write(_darkModeKey, enabled);
-    isDarkMode = enabled;
+    await _storage.write(_darkModeKey, enabled);
+    _isDarkMode = enabled;
   }
 
   Future<void> languageUpdate(String lang) async {
-    await storage.write(_lanKey, lang);
-    currentLanguage = lang;
+    await _storage.write(_lanKey, lang);
+    _currentLanguage = lang;
   }
 
   Future<void> networkUpdate(String name) async {
-    await storage.write(_networkKey, name);
-    currentNetwork = name;
+    await _storage.write(_networkKey, name);
+    _currentNetwork = name;
   }
 
   Future<void> testnetHiddenUpdate(bool enabled) async {
-    await storage.write(_testnetHiddenKey, enabled);
-    isTestnetHidden = enabled;
+    await _storage.write(_testnetHiddenKey, enabled);
+    _isTestnetHidden = enabled;
   }
 }
