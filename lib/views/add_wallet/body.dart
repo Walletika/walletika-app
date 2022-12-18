f5449af6ee2584247279f8bd29bc16a7b283276c
Mahmoud Khalid <darkness20016@gmail.com>
2022-12-18 14:11:11 +0200

    AddWallet view upgrade

--- lib/views/add_wallet/body.dart
+++ lib/views/add_wallet/body.dart
@@ -6,7 +6,6 @@ import 'package:line_icons/line_icons.dart';
 
 import '../../controllers/wallet/wallet.dart';
 import '../../utils/constants.dart';
-import '../widgets/container.dart';
 import '../widgets/custom_text_field.dart';
 import '../widgets/dialog.dart';
 import '../widgets/operation_notifier.dart';
@@ -26,132 +25,122 @@ class AddWalletBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const ContainerWithShadow(),
-        Expanded(
-          child: Form(
-            key: _formController,
-            child: ListView(
-              padding: const EdgeInsets.all(AppDecoration.paddingBig),
-              cacheExtent: 1000,
-              children: [
-                Text(
-                  "1000@addWallet".tr,
-                  style: Theme.of(context).textTheme.titleSmall,
-                  textAlign: TextAlign.center,
-                ),
-                verticalSpace(AppDecoration.spaceBig),
-                Center(
-                  child: SizedBox(
-                    width: AppDecoration.widgetWidth,
-                    child: Text("1001@addWallet".tr),
-                  ),
-                ),
-                verticalSpace(),
-                CustomTextFormField(
-                  controller: _usernameController,
-                  placeholderText: "1010@global".tr,
-                  prefixIcon: const Icon(LineIcons.user),
-                  keyboardType: TextInputType.name,
-                  inputFormatters: [
-                    FilteringTextInputFormatter.allow(
-                      AppRegExp.lettersWithoutSpace,
-                    ),
-                  ],
-                  validator: (text) {
-                    if (text != null &&
-                        _walletController.usernameExists(text)) {
-                      return "1018@global".tr;
-                    }
-                    return null;
-                  },
-                  onEditingComplete: _onSubmit,
-                ),
-                verticalSpace(),
-                Center(
-                  child: SizedBox(
-                    width: AppDecoration.widgetWidth,
-                    child: Text("1002@addWallet".tr),
-                  ),
-                ),
-                verticalSpace(),
-                PasswordFormField(
-                  controller: _passwordController,
-                  placeholderText: "1011@global".tr,
-                  onEditingComplete: _onSubmit,
-                ),
-                verticalSpace(),
-                PasswordFormField(
-                  controller: _confirmPassController,
-                  placeholderText: "1012@global".tr,
-                  enableStrengthBar: false,
-                  validator: (text) {
-                    if (text != null && text != _passwordController.text) {
-                      return "1017@global".tr;
-                    }
-                    return null;
-                  },
-                  onEditingComplete: _onSubmit,
-                ),
-                verticalSpace(),
-                Center(
-                  child: SizedBox(
-                    width: AppDecoration.widgetWidth,
-                    child: Text("1003@addWallet".tr),
-                  ),
-                ),
-                verticalSpace(),
-                PasswordFormField(
-                  controller: _securityPassController,
-                  placeholderText: "1013@global".tr,
-                  validator: (text) {
-                    if (text != null && text == _passwordController.text) {
-                      return "1023@global".tr;
-                    }
-                    return null;
-                  },
-                  onEditingComplete: _onSubmit,
-                ),
-                verticalSpace(),
-                PasswordFormField(
-                  controller: _confirmSPassController,
-                  placeholderText: "1014@global".tr,
-                  enableStrengthBar: false,
-                  validator: (text) {
-                    if (text != null && text != _securityPassController.text) {
-                      return "1017@global".tr;
-                    }
-                    return null;
-                  },
-                  onEditingComplete: _onSubmit,
-                ),
-                verticalSpace(),
-                Center(
-                  child: SizedBox(
-                    width: AppDecoration.widgetWidth,
-                    height: AppDecoration.buttonHeightLarge,
-                    child: ElevatedButton(
-                      onPressed: _onSubmit,
-                      child: Text("1004@addWallet".tr),
-                    ),
-                  ),
-                ),
-              ],
+    return Form(
+      key: _formController,
+      child: ListView(
+        padding: const EdgeInsets.all(AppDecoration.paddingMedium),
+        cacheExtent: 1000,
+        children: [
+          Text(
+            "1000@addWallet".tr,
+            style: Theme.of(context).textTheme.titleSmall,
+            textAlign: TextAlign.center,
+          ),
+          verticalSpace(AppDecoration.spaceBig),
+          Center(
+            child: SizedBox(
+              width: AppDecoration.widgetWidth,
+              child: Text("1001@addWallet".tr),
             ),
           ),
-        ),
-      ],
+          verticalSpace(),
+          CustomTextFormField(
+            controller: _usernameController,
+            placeholderText: "1010@global".tr,
+            prefixIcon: const Icon(LineIcons.user),
+            keyboardType: TextInputType.name,
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(AppRegExp.lettersWithoutSpace),
+            ],
+            validator: (text) {
+              if (text != null && _walletController.usernameExists(text)) {
+                return "1018@global".tr;
+              }
+              return null;
+            },
+            onEditingComplete: _onSubmit,
+          ),
+          verticalSpace(),
+          Center(
+            child: SizedBox(
+              width: AppDecoration.widgetWidth,
+              child: Text("1002@addWallet".tr),
+            ),
+          ),
+          verticalSpace(),
+          PasswordFormField(
+            controller: _passwordController,
+            placeholderText: "1011@global".tr,
+            onEditingComplete: _onSubmit,
+          ),
+          verticalSpace(),
+          PasswordFormField(
+            controller: _confirmPassController,
+            placeholderText: "1012@global".tr,
+            enableStrengthBar: false,
+            validator: (text) {
+              if (text != null && text != _passwordController.text) {
+                return "1017@global".tr;
+              }
+              return null;
+            },
+            onEditingComplete: _onSubmit,
+          ),
+          verticalSpace(),
+          Center(
+            child: SizedBox(
+              width: AppDecoration.widgetWidth,
+              child: Text("1003@addWallet".tr),
+            ),
+          ),
+          verticalSpace(),
+          PasswordFormField(
+            controller: _securityPassController,
+            placeholderText: "1013@global".tr,
+            validator: (text) {
+              if (text != null && text == _passwordController.text) {
+                return "1023@global".tr;
+              }
+              return null;
+            },
+            onEditingComplete: _onSubmit,
+          ),
+          verticalSpace(),
+          PasswordFormField(
+            controller: _confirmSPassController,
+            placeholderText: "1014@global".tr,
+            enableStrengthBar: false,
+            validator: (text) {
+              if (text != null && text != _securityPassController.text) {
+                return "1017@global".tr;
+              }
+              return null;
+            },
+            onEditingComplete: _onSubmit,
+          ),
+          verticalSpace(),
+          Center(
+            child: SizedBox(
+              width: AppDecoration.widgetWidth,
+              height: AppDecoration.buttonHeightLarge,
+              child: ElevatedButton(
+                onPressed: _onSubmit,
+                child: Text("1004@addWallet".tr),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
   void _onSubmit() {
     if (_formController.currentState!.validate()) {
-      _onAddNew();
+      _addWallet();
     }
   }
 
-  void _onAddNew() {
+  void _addWallet() {
     final OperationNotifier operation = OperationNotifier(
       title: "1004@addWallet".tr,
     );
@@ -166,10 +155,7 @@ class AddWalletBody extends StatelessWidget {
       if (isValid) {
         Get.back();
 
-        final BuildContext context = Get.context!;
-
         awesomeDialog(
-          context: context,
           dialogType: DialogType.success,
           body: Padding(
             padding: const EdgeInsets.only(left: AppDecoration.padding),
@@ -179,7 +165,7 @@ class AddWalletBody extends StatelessWidget {
                 Center(
                   child: Text(
                     "1021@global".tr,
-                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    style: Theme.of(Get.context!).textTheme.bodyLarge!.copyWith(
                           fontFamily: AppFonts.medium,
                           color: AppColors.green,
                         ),
@@ -188,46 +174,41 @@ class AddWalletBody extends StatelessWidget {
                 verticalSpace(AppDecoration.spaceMedium),
                 Text(
                   "1005@addWallet".tr,
-                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  style: Theme.of(Get.context!).textTheme.bodyLarge!.copyWith(
                         color: Colors.red,
                       ),
                 ),
                 verticalSpace(),
                 Text(
                   "1006@addWallet".tr,
-                  style: Theme.of(context).textTheme.labelSmall,
+                  style: Theme.of(Get.context!).textTheme.labelSmall,
                 ),
                 verticalSpace(),
                 Text(
                   "1007@addWallet".tr,
-                  style: Theme.of(context).textTheme.labelSmall,
+                  style: Theme.of(Get.context!).textTheme.labelSmall,
                 ),
                 verticalSpace(),
                 Text(
                   "1008@addWallet".tr,
-                  style: Theme.of(context).textTheme.labelSmall,
+                  style: Theme.of(Get.context!).textTheme.labelSmall,
                 ),
                 verticalSpace(),
                 Text(
                   "1009@addWallet".tr,
-                  style: Theme.of(context).textTheme.labelSmall,
+                  style: Theme.of(Get.context!).textTheme.labelSmall,
                 ),
                 verticalSpace(),
                 Text(
                   "1010@addWallet".tr,
-                  style: Theme.of(context).textTheme.labelSmall,
+                  style: Theme.of(Get.context!).textTheme.labelSmall,
                 ),
                 verticalSpace(),
               ],
             ),
           ),
           btnOkText: "1012@addWallet".tr,
-          btnOkOnPress: () {
-            Future.delayed(
-              const Duration(milliseconds: 500),
-              () => Get.toNamed(AppPages.authSetup),
-            );
-          },
+          btnOkOnPress: () => Get.toNamed(AppPages.authSetup),
         ).show();
       } else {
         operation.invalid("1011@addWallet".tr);
