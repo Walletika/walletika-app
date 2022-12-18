a7fd9e8273127f985a346ad00313dac974ceffad
Mahmoud Khalid <darkness20016@gmail.com>
2022-12-18 12:35:40 +0200

    PinputFormField auto dispose

--- lib/views/widgets/pin_field.dart
+++ lib/views/widgets/pin_field.dart
@@ -39,6 +39,13 @@ class _PinputFormFieldState extends State<PinputFormField> {
   final FocusNode _focusNode = FocusNode();
 
   @override
+  void dispose() {
+    widget.controller.dispose();
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final defaultPinTheme = PinTheme(
       width: 40.0,
