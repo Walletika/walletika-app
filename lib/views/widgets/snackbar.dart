1d3f573db6cdf96f655bfc0cc546accc50377208
Mahmoud Khalid <darkness20016@gmail.com>
2022-12-24 17:20:36 +0200

    modernSnackBar title optional

--- lib/views/widgets/snackbar.dart
+++ lib/views/widgets/snackbar.dart
@@ -5,11 +5,11 @@ import 'package:line_icons/line_icons.dart';
 import '../../utils/constants.dart';
 
 SnackbarController modernSnackBar({
-  required String title,
-  required String message,
   Icon? icon,
+  String? title,
+  String? message,
   bool? isSuccess,
-  double maxWidth = 500.0,
+  double maxWidth = 400.0,
   void Function(GetSnackBar)? onTap,
 }) {
   if (isSuccess != null) {
@@ -26,32 +26,38 @@ SnackbarController modernSnackBar({
           );
   }
 
-  return Get.snackbar(
-    '',
-    '',
+  final TextTheme textTheme = Theme.of(Get.context!).textTheme;
+
+  return Get.rawSnackbar(
     backgroundColor: Colors.black87,
     leftBarIndicatorColor: icon?.color,
     barBlur: 0,
-    titleText: Text(
-      title,
-      style: Theme.of(Get.context!).textTheme.titleSmall!.copyWith(
-            color: icon?.color ?? Colors.white,
-          ),
-    ),
-    messageText: Text(
-      message,
-      style: Theme.of(Get.context!).textTheme.bodySmall!.copyWith(
-            color: icon?.color ?? Colors.white,
-          ),
+    maxWidth: maxWidth,
+    snackPosition: SnackPosition.TOP,
+    borderRadius: AppDecoration.radius,
+    margin: const EdgeInsets.all(AppDecoration.margin),
+    padding: EdgeInsets.symmetric(
+      horizontal: AppDecoration.paddingMedium,
+      vertical:
+          title != null ? AppDecoration.padding : AppDecoration.paddingMedium,
     ),
     icon: icon,
-    maxWidth: maxWidth,
-    padding: const EdgeInsets.symmetric(
-      horizontal: AppDecoration.paddingMedium,
-      vertical: AppDecoration.padding,
-    ),
-    margin: const EdgeInsets.all(AppDecoration.margin),
-    borderRadius: AppDecoration.radius,
+    titleText: title != null
+        ? Text(
+            title,
+            style: textTheme.titleSmall!.copyWith(
+              color: icon?.color ?? Colors.white,
+            ),
+          )
+        : null,
+    messageText: message != null
+        ? Text(
+            message,
+            style: textTheme.bodySmall!.copyWith(
+              color: icon?.color ?? Colors.white,
+            ),
+          )
+        : null,
     onTap: onTap,
   );
 }
