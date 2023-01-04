55a01208c349d84d2906d513cabd78a14db29ca3
Mahmoud Khalid <darkness20016@gmail.com>
2023-01-04 10:02:28 +0200

    Added AddressesBookController to AppBinding

--- lib/controllers/dependencies.dart
+++ lib/controllers/dependencies.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 
+import 'addresses_book/addresses_book.dart';
 import 'network/network.dart';
 import 'settings/settings.dart';
 import 'wallet/wallet.dart';
@@ -10,5 +11,6 @@ class AppBinding extends Bindings {
     Get.put(WalletController(), permanent: true);
     Get.put(SettingsController(), permanent: true);
     Get.put(NetworkController(), permanent: true);
+    Get.put(AddressesBookController(), permanent: true);
   }
 }
